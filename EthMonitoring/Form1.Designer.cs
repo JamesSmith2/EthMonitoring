526d64a2463b493bfb7442861574c8a18f5b74c4
Jaanus Lepik <jaanus.lepik@kiuto.com>
2017-05-25 16:00:08 +0300

    Add project files.

---
+++ EthMonitoring/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace EthMonitoring
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startMonitoring = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tokenField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hostField = new System.Windows.Forms.TextBox();
+            this.addhost = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tokenLink = new System.Windows.Forms.LinkLabel();
+            this.hostsList = new System.Windows.Forms.ListView();
+            this.host = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.eth_hashrate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dcr_hr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.temp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.version = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hostName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.removeItem = new System.Windows.Forms.Button();
+            this.clearList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startMonitoring
+            // 
+            this.startMonitoring.Location = new System.Drawing.Point(567, 12);
+            this.startMonitoring.Name = "startMonitoring";
+            this.startMonitoring.Size = new System.Drawing.Size(138, 36);
+            this.startMonitoring.TabIndex = 0;
+            this.startMonitoring.Text = "Start monitoring";
+            this.startMonitoring.UseVisualStyleBackColor = true;
+            this.startMonitoring.Click += new System.EventHandler(this.startMonitoring_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Servers for monitoring";
+            // 
+            // tokenField
+            // 
+            this.tokenField.Location = new System.Drawing.Point(12, 28);
+            this.tokenField.Name = "tokenField";
+            this.tokenField.Size = new System.Drawing.Size(549, 20);
+            this.tokenField.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Access token";
+            // 
+            // hostField
+            // 
+            this.hostField.Location = new System.Drawing.Point(12, 67);
+            this.hostField.Name = "hostField";
+            this.hostField.Size = new System.Drawing.Size(112, 20);
+            this.hostField.TabIndex = 5;
+            // 
+            // addhost
+            // 
+            this.addhost.Location = new System.Drawing.Point(265, 65);
+            this.addhost.Name = "addhost";
+            this.addhost.Size = new System.Drawing.Size(75, 23);
+            this.addhost.TabIndex = 6;
+            this.addhost.Text = "Add host";
+            this.addhost.UseVisualStyleBackColor = true;
+            this.addhost.Click += new System.EventHandler(this.addhost_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Host";
+            // 
+            // tokenLink
+            // 
+            this.tokenLink.AutoSize = true;
+            this.tokenLink.Location = new System.Drawing.Point(554, 70);
+            this.tokenLink.Name = "tokenLink";
+            this.tokenLink.Size = new System.Drawing.Size(151, 13);
+            this.tokenLink.TabIndex = 8;
+            this.tokenLink.TabStop = true;
+            this.tokenLink.Text = "Get your auth token from here!";
+            // 
+            // hostsList
+            // 
+            this.hostsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.host,
+            this.name,
+            this.eth_hashrate,
+            this.dcr_hr,
+            this.temp,
+            this.version});
+            this.hostsList.Location = new System.Drawing.Point(12, 106);
+            this.hostsList.Name = "hostsList";
+            this.hostsList.Size = new System.Drawing.Size(693, 253);
+            this.hostsList.TabIndex = 9;
+            this.hostsList.UseCompatibleStateImageBehavior = false;
+            this.hostsList.View = System.Windows.Forms.View.Details;
+            // 
+            // host
+            // 
+            this.host.Text = "Host";
+            this.host.Width = 110;
+            // 
+            // name
+            // 
+            this.name.Text = "Miner name";
+            this.name.Width = 113;
+            // 
+            // eth_hashrate
+            // 
+            this.eth_hashrate.Text = "ETH Hashrate";
+            this.eth_hashrate.Width = 80;
+            // 
+            // dcr_hr
+            // 
+            this.dcr_hr.Text = "DCR Hashrate";
+            this.dcr_hr.Width = 88;
+            // 
+            // temp
+            // 
+            this.temp.Text = "Temperatures";
+            this.temp.Width = 142;
+            // 
+            // version
+            // 
+            this.version.Text = "Version";
+            // 
+            // hostName
+            // 
+            this.hostName.Location = new System.Drawing.Point(130, 67);
+            this.hostName.Name = "hostName";
+            this.hostName.Size = new System.Drawing.Size(129, 20);
+            this.hostName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(127, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Name";
+            // 
+            // removeItem
+            // 
+            this.removeItem.Location = new System.Drawing.Point(12, 365);
+            this.removeItem.Name = "removeItem";
+            this.removeItem.Size = new System.Drawing.Size(150, 24);
+            this.removeItem.TabIndex = 12;
+            this.removeItem.Text = "Remove selected miner";
+            this.removeItem.UseVisualStyleBackColor = true;
+            this.removeItem.Click += new System.EventHandler(this.removeItem_Click);
+            // 
+            // clearList
+            // 
+            this.clearList.Location = new System.Drawing.Point(169, 366);
+            this.clearList.Name = "clearList";
+            this.clearList.Size = new System.Drawing.Size(103, 23);
+            this.clearList.TabIndex = 13;
+            this.clearList.Text = "Clear list";
+            this.clearList.UseVisualStyleBackColor = true;
+            this.clearList.Click += new System.EventHandler(this.clearList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 398);
+            this.Controls.Add(this.clearList);
+            this.Controls.Add(this.removeItem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hostName);
+            this.Controls.Add(this.hostsList);
+            this.Controls.Add(this.tokenLink);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addhost);
+            this.Controls.Add(this.hostField);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tokenField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startMonitoring);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Dual miner monitoring";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startMonitoring;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tokenField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox hostField;
+        private System.Windows.Forms.Button addhost;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel tokenLink;
+        private System.Windows.Forms.ListView hostsList;
+        private System.Windows.Forms.ColumnHeader host;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader eth_hashrate;
+        private System.Windows.Forms.TextBox hostName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ColumnHeader dcr_hr;
+        private System.Windows.Forms.ColumnHeader temp;
+        private System.Windows.Forms.ColumnHeader version;
+        private System.Windows.Forms.Button removeItem;
+        private System.Windows.Forms.Button clearList;
+    }
+}
+
---
+++ EthMonitoring/Form1.cs
@@ -0,0 +1,338 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EthMonitoring
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker bw;
+        private Boolean Monitoring = false;
+        private MySettings settings = MySettings.Load();
+
+        class EthMonJsonTemplate
+        {
+            public int id { get; set; }
+            public string error { get; set; }
+            public List<string> result { get; set; }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+    
+            this.bw = new BackgroundWorker();
+            this.bw.DoWork += new DoWorkEventHandler(monitoringHosts);
+
+            // Generate dictonary if needed
+            if (settings.hosts == null)
+            {
+                settings.hosts = new Dictionary<string, string>();
+            }
+
+            // Set token value
+            tokenField.Text = settings.accessToken;
+
+            if (settings.hosts != null && settings.hosts.Count > 0)
+            {
+                foreach(KeyValuePair<string, string> entry in settings.hosts)
+                {
+                    ListViewItem newHost = new ListViewItem(entry.Value);
+
+                    newHost.SubItems.Add(entry.Key); // Name
+                    newHost.SubItems.Add("Initializing.."); // ETH HASHRATE
+                    newHost.SubItems.Add("Initializing.."); // DCR HASHRATE
+                    newHost.SubItems.Add("Initializing.."); // TEMPERATURES
+                    newHost.SubItems.Add(""); // VERSION
+
+                    hostsList.Items.Add(newHost);
+                }
+
+                if(tokenField.Text.Length > 0)
+                {
+                    startMonitoringMiners();
+                }
+            }
+        }
+
+        private void addhost_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (hostField.Text != "" && hostName.Text != "")
+                {
+                    ListViewItem newHost = new ListViewItem(hostField.Text);
+
+                    newHost.SubItems.Add(hostName.Text); // Hostname
+                    newHost.SubItems.Add("Initializing.."); // ETH HASHRATE
+                    newHost.SubItems.Add("Initializing.."); // DCR HASHRATE
+                    newHost.SubItems.Add("Initializing.."); // TEMPERATURES
+                    newHost.SubItems.Add(""); // VERSION
+
+                    hostsList.Items.Add(newHost);
+
+                    settings.hosts.Add(hostName.Text, hostField.Text);
+                    settings.Save();
+                }
+            } catch(Exception ex)
+            {
+                Console.WriteLine("Exception on addHost: " + ex.Message);
+            }
+        }
+
+        /**
+         * Updates webservice api
+         * 
+         */
+        private void sendAPIUpdate(string _data, string _host, string _name)
+        {
+            string serviceToken = tokenField.Text;
+
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["token"] = tokenField.Text;
+                values["data"] = _data.Trim();
+                values["host"] = _host;
+                values["name"] = _name;
+
+                var response = client.UploadValues("http://monitoring.mylifegadgets.com/api/update", "POST", values);
+
+                //var responseString = Encoding.Default.GetString(response);
+
+                //Console.WriteLine(responseString);
+            }
+        }
+
+        private void monitoringHosts(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+            while (this.Monitoring)
+            {
+                try
+                {
+                    // Get list from listview
+                    if (hostsList.Items.Count > 0)
+                    {
+                        foreach (ListViewItem hostRow in this.hostsList.Items)
+                        {
+                            var clientSocket = new System.Net.Sockets.TcpClient();
+                            string host = hostRow.SubItems[0].Text;
+                            string name = hostRow.SubItems[1].Text;
+                            Boolean status = false;
+
+                            //Console.WriteLine("Getting host: " + host); 
+                            //clientSocket.Connect(host, 3333);
+                            if (clientSocket.ConnectAsync(host, 3333).Wait(1000))
+                            {
+                                status = true;
+                                string get_menu_request = "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"miner_getstat1\"}";
+                                NetworkStream serverStream = clientSocket.GetStream();
+                                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(get_menu_request);
+                                serverStream.Write(outStream, 0, outStream.Length);
+                                serverStream.Flush();
+
+                                byte[] inStream = new byte[clientSocket.ReceiveBufferSize];
+                                serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+                                string _returndata = System.Text.Encoding.ASCII.GetString(inStream);
+
+                                EthMonJsonTemplate result = JsonConvert.DeserializeObject<EthMonJsonTemplate>(_returndata);
+
+                                hostRow.SubItems[5].Text = result.result[0]; // Version
+                                                                             // ETH Hashrates
+                                string[] eth_hashrates = result.result[3].Split(';');
+                                string eth_hashrate = "";
+
+                                for (int i = 0; i < eth_hashrates.Length; i++)
+                                {
+                                    double hashrate = Double.Parse(eth_hashrates[i]) / 1000;
+
+                                    eth_hashrate += "GPU" + i + ": " + hashrate.ToString() + "Mh/s "; // Temps
+                                }
+
+                                hostRow.SubItems[2].Text = eth_hashrate; // ETH HR
+
+                                // DCR Hashrates
+                                string[] dcr_hashrates = result.result[5].Split(';');
+                                string dcr_hashrate = "";
+                                if (dcr_hashrates[0] == "off")
+                                {
+                                    hostRow.SubItems[3].Text = "Mode 1 activated";
+                                }
+                                else
+                                {
+                                    for (int i = 0; i < dcr_hashrates.Length; i++)
+                                    {
+                                        double hashrate = Double.Parse(dcr_hashrates[i]) / 1000;
+
+                                        dcr_hashrate += "GPU" + i + ": " + hashrate.ToString() + "Mh/s "; // Temps
+                                    }
+
+                                    hostRow.SubItems[3].Text = dcr_hashrate; // DCR HR
+                                }
+
+                                // Temps
+                                string[] temp = result.result[6].Split(';');
+                                string temps = "";
+
+                                for (int i = 0; i < temp.Length; i++)
+                                {
+                                    temps += "GPU" + i + ": " + temp[i] + "C (FAN: " + temp[i + 1] + "%) "; // Temps
+                                    i++;
+                                }
+
+                                hostRow.SubItems[4].Text = temps;
+
+                                // Close socket
+                                clientSocket.Close();
+                                clientSocket = null;
+
+                                this.hostsList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                                this.hostsList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+                                // Update web database for SMS Services
+                                sendAPIUpdate(_returndata, host, name);
+                            } else
+                            {
+                                // Update web database for SMS Services
+                                sendAPIUpdate("", host, name);
+
+                                // Set values
+                                hostRow.SubItems[2].Text = "OFFLINE";
+                                hostRow.SubItems[3].Text = "OFFLINE";
+                                hostRow.SubItems[4].Text = "OFFLINE";
+                                hostRow.SubItems[5].Text = "OFFLINE";
+                            }
+
+                            // Print
+                            Console.WriteLine("Host: " + host + " updated");
+                        }
+
+
+                    } else
+                    {
+                        Console.WriteLine("List empty");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                    Console.WriteLine("Socket error...");
+                }
+
+                // Collect free memory
+                GC.Collect();
+
+                // Sleep for next reading
+                System.Threading.Thread.Sleep(2000);
+            }
+
+            Console.WriteLine("Monitoring stopped..");
+        }
+
+        private void startMonitoringMiners()
+        {
+            if (!this.bw.IsBusy && this.Monitoring == false)
+            {
+                this.Monitoring = true;
+                this.bw.RunWorkerAsync();
+                this.startMonitoring.Text = "Stop monitoring";
+
+                // Save token
+                settings.accessToken = tokenField.Text;
+                settings.Save();
+            }
+            else
+            {
+                this.Monitoring = false;
+                this.startMonitoring.Text = "Start monitoring";
+            }
+        }
+
+        private void startMonitoring_Click(object sender, EventArgs e)
+        {
+            startMonitoringMiners();
+        }
+
+        class MySettings : AppSettings<MySettings>
+        {
+            public Dictionary<string, string> hosts = null;
+            public string accessToken = "";
+        }
+
+        public class AppSettings<T> where T : new()
+        {
+            private const string DEFAULT_FILENAME = "settings.json";
+
+            public void Save(string fileName = DEFAULT_FILENAME)
+            {
+                File.WriteAllText(fileName, JsonConvert.SerializeObject(this));
+            }
+
+            public static void Save(T pSettings, string fileName = DEFAULT_FILENAME)
+            {
+                File.WriteAllText(fileName, JsonConvert.SerializeObject(pSettings));
+            }
+
+            public static T Load(string fileName = DEFAULT_FILENAME)
+            {
+                T t = new T();
+                if (File.Exists(fileName))
+                    t = (JsonConvert.DeserializeObject<T>(File.ReadAllText(fileName)));
+                return t;
+            }
+        }
+
+        private void removeItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.hostsList.SelectedItems.Count > 0)
+                {
+                    if (settings.hosts.Count > 0)
+                    {
+                        foreach (KeyValuePair<string, string> entry in settings.hosts.ToList())
+                        {
+                            if (this.hostsList.SelectedItems[0].SubItems[0].Text == entry.Value)
+                            {
+                                settings.hosts.Remove(entry.Key);
+                            }
+                        }
+                    }
+
+                    this.hostsList.Items.Remove(this.hostsList.SelectedItems[0]);
+
+                    settings.Save();
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void clearList_Click(object sender, EventArgs e)
+        {
+            if(this.Monitoring)
+            {
+                startMonitoringMiners();
+            }
+ 
+            this.hostsList.Clear();
+            settings.hosts.Clear();
+            settings.Save();
+        }
+    }
+}
